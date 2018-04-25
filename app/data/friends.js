d38fdfcaa48d09656e91954981a9e0eb0e1ae42c
Vizena2005 <vizena2005@gmail.com>
2018-04-25 14:59:04 -0500

    pushing to heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,21 @@
-$(document).ready(function () {
-
-
-
-
-
-
-
-});
+var userArr = [{
+    name: "Bob",
+    photo: "https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg",
+    scores: [1, 3, 2, 4, 5, 3, 2, 2, 5, 4]
+},
+{
+    name: "Chris",
+    photo: "https://s4.reutersmedia.net/resources/r/?m=02&d=20171018&t=2&i=1206067272&w=&fh=545&fw=810&ll=&pl=&sq=&r=2017-10-18T190508Z_7738_MRPRC14191C48B0_RTRMADP_0_RUSSIA-ANIMALS",
+    scores: [1, 2, 2, 4, 3, 3, 2, 2, 3, 5]
+},
+{
+    name: "Brant",
+    photo: "https://images.pexels.com/photos/207962/pexels-photo-207962.jpeg?auto=compress&cs=tinysrgb&h=350",
+    scores: [1, 2, 2, 1, 1, 1, 1, 1, 3, 5]
+},
+{
+    name: "Juhi",
+    photo: "https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/5a9acf819140b7602d66bb39/1520095109871/the-emoji-movie-wins-worst-picture-at-38th-razzie-awards-heres-the-full-list-of-winners-or-losers-social.jpg?format=750w",
+    scores: [1, 5, 5, 5, 5, 5, 2, 2, 3, 5]
+}
+];
